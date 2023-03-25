3cf32b4b906e6e1f84d2fe47e86ee2ff3a80b6f5
carraes <carraeshb@gmail.com>
2023-03-25 16:57:43 -0300

    feat: app struct, connect/routes with middleware setup

---
+++ backend/controllers/routes.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/carlosarraes/qcmeback/models"
+	"github.com/carlosarraes/qcmeback/utils"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type App struct {
+	DSN string
+	DB  models.Data
+}
+
+func (a *App) Connect() (*sql.DB, error) {
+	db, err := models.OpenDB(a.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (a *App) Routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(utils.Cors())
+
+	mux.Get("/", a.GenerateQRCode)
+
+	return mux
+}
