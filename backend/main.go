34afc4dbabadd0955a4121f61fc79a9cc3741d48
carraes <carraeshb@gmail.com>
2023-03-25 17:00:04 -0300

    feat: main structure, connects to DB, set up routes, runs server

---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/carlosarraes/qcmeback/controllers"
+	"github.com/carlosarraes/qcmeback/models"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	port := 8080
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	app := controllers.App{}
+	flag.StringVar(&app.DSN, "dsn", os.Getenv("DB_URI"), "Postgres DSN")
+	flag.Parse()
+
+	conn, err := app.Connect()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	defer conn.Close()
+
+	app.DB = &models.Postgress{DB: conn}
+
+	server := app.Routes()
+
+	log.Printf("Server listening on port %d", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
+}
