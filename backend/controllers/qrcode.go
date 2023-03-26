48ebfc94375b20b06f0c147204300f8e693901fe
carraes <carraeshb@gmail.com>
2023-03-25 22:44:08 -0300

    feat[controllers]: create basic qrcode generation for testing

--- backend/controllers/qrcode.go
+++ backend/controllers/qrcode.go
@@ -1,9 +1,29 @@
 package controllers
 
-import "net/http"
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/skip2/go-qrcode"
+)
+
+var url = "https://qrcodeme.herokuapp.com/qrcodeme/"
 
 func (a *App) GenerateQRCode(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
+	name := strings.ToLower(chi.URLParam(r, "name"))
+
+	png, err := qrcode.Encode(url+name, qrcode.Medium, 256)
+	if err != nil {
+		log.Fatalf("Failed to generate QR Code: %v", err)
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Content-Disposition", "attachment; filename=qrcode.png")
+
+	buf := bytes.NewReader(png)
+	http.ServeContent(w, r, "qrcode.png", time.Time{}, buf)
 }
