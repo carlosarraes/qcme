2bddaf6b91fd6b058b9edd372bfb24d95d231d6a
carraes <carraeshb@gmail.com>
2023-03-26 00:07:04 -0300

    feat[controllers]: add createuser to qrcodeme route, create getUser controller

--- backend/controllers/qrcode.go
+++ backend/controllers/qrcode.go
@@ -3,13 +3,13 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
-	"log"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/carlosarraes/qcmeback/models"
 	"github.com/carlosarraes/qcmeback/utils"
+	"github.com/go-chi/chi/v5"
 )
 
 const url = "https://qrcodeme.herokuapp.com/qrcodeme/"
@@ -20,7 +20,6 @@ func (a *App) GenerateQRCode(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	log.Println(user.Name)
 
 	buffer, err := utils.DrawQrCode(url, strings.ToLower(user.Name))
 	if err != nil {
@@ -28,7 +27,24 @@ func (a *App) GenerateQRCode(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if err := a.DB.CreateUser(user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "image/png")
 	w.Header().Set("Content-Disposition", "attachment; filename=qrcode.png")
 	http.ServeContent(w, r, "qrcode.png", time.Time{}, bytes.NewReader(buffer.Bytes()))
+}
+
+func (a *App) GetUser(w http.ResponseWriter, r *http.Request) {
+	name := chi.URLParam(r, "name")
+	user, err := a.DB.GetUser(name)
+	if err != nil {
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
