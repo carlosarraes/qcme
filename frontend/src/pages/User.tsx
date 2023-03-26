17e7ac7111c175cb13fbd316bd2f44e5544332d1
carraes <carraeshb@gmail.com>
2023-03-26 15:48:33 -0300

    refactor[front]: sets loading when getting data, better context on the text

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -2,10 +2,12 @@ import { useParams } from 'react-router-dom'
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 import { useEffect, useState } from 'react'
 import { URL, UserType } from './Home'
+import Spinner from '../components/Spinner'
 
 const User = () => {
   const [user, setUser] = useState<UserType | null>(null)
   const [notFound, setNotFound] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(true)
   const { name } = useParams<{ name: string }>()
 
   useEffect(() => {
@@ -18,7 +20,9 @@ const User = () => {
         }
         const data = await response.json()
         setUser(data)
+        setLoading(false)
       } catch (e) {
+        setLoading(false)
         setNotFound(true)
       }
     }
@@ -26,12 +30,16 @@ const User = () => {
     fetchData()
   }, [])
 
+  if (loading) {
+    return <Spinner />
+  }
+
   if (notFound) {
     return (
       <main className="flex h-full w-11/12 max-w-sm mx-auto justify-center items-center">
         <section className="w-full flex flex-col items-start p-4 border-2 border-slate-400 rounded-md shadow-lg">
           <h2 className="text-2xl">
-            User <span className="font-bold capitalize">{name}</span> not found :(
+            User <span className="font-bold capitalize">{name}</span> was not found :(
           </h2>
         </section>
       </main>
@@ -41,10 +49,11 @@ const User = () => {
   return (
     <main className="flex h-full w-11/12 max-w-sm mx-auto justify-center items-center">
       <section className="w-full flex flex-col items-start p-4 border-2 border-slate-400 rounded-md shadow-lg">
-        <h2 className="text-2xl">
-          Hello, <span className="font-bold capitalize">{user?.name}</span> :)
-        </h2>
-        <p className="text-lg mt-6">Here are the links to your social media accounts:</p>
+        <h2 className="text-2xl">Hello! :)</h2>
+        <p className="text-md mt-6">
+          Here are the links to <span className="font-bold capitalize">{user?.name}</span> social
+          media accounts:
+        </p>
         <div className="flex justify-between w-full mt-6">
           <a
             href={user?.linkedIn}
