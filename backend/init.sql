bf247f553dda98f171805f0bc5eaf8635b759523
carraes <carraeshb@gmail.com>
2023-03-26 15:33:25 -0300

    refactor[sql]: change varchar to 512

--- backend/init.sql
+++ backend/init.sql
@@ -3,6 +3,6 @@ CREATE SCHEMA IF NOT EXISTS data;
 CREATE TABLE data.qrcode (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    linkedin VARCHAR(2048),
-    github VARCHAR(2048)
+    linkedin VARCHAR(512),
+    github VARCHAR(512)
 );
