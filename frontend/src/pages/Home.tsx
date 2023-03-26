8baf678e7b33b63c44ea498ecf07a59a76824bb6
carraes <carraeshb@gmail.com>
2023-03-26 15:10:43 -0300

    feat[front]: connection with containerized backend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-export const URL = 'http://localhost:8080/'
+export const URL = 'https://goqr-4wgfen3n5q-rj.a.run.app/'
 
 export type UserType = {
   name: string
