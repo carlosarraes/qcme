932a84137277c25713193355bd10a68ab44879b7
carraes <carraeshb@gmail.com>
2023-03-26 23:16:51 -0300

    refactor[front]: handlers basic response for post

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,5 +1,16 @@
 import { rest } from 'msw'
 
-const url = ''
+const url = 'https://goqr-4wgfen3n5q-rj.a.run.app/'
 
-export const handlers = []
+export const handlers = [
+  rest.post(url + 'qrcodeme', (req, res, ctx) => {
+    const { name } = req.body as { name: string }
+
+    if (name === 'existingUser') {
+      return res(ctx.status(400), ctx.json({ error: 'User already exists!' }))
+    }
+
+    const fakeQrCodeBlob = new Blob(['fakeQrCode'], { type: 'image/png' })
+    return res(ctx.status(200), ctx.body(fakeQrCodeBlob))
+  }),
+]
